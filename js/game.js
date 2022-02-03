a3f5316a3abf29cee4419079b81cbe4b9df313e2
josselinguajala <63566094+josselinguajala@users.noreply.github.com>
2022-02-03 16:25:44 -0500

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,25 @@
+const WIDTH  = 400;
+const HEIGHT = 400;
+
+let target = {
+    x: getRandomNumber(WIDTH),
+    y: getRandomNumber(HEIGHT)
+};
+
+let $map = document.getElementById('map');
+let $distance = document.getElementById('distance');
+let clicks = 0;
+
+$map.addEventListener('click', function (e) {
+    clicks++;
+    let distance = getDistance(e,target);
+    let distanceHint = getDistanceHint(distance);
+    $distance.innerHTML=  `<h1>${distanceHint}</h1>`
+
+    if (distance < 20) {
+       
+       alert( `Tesoro encontrado en ${clicks} clicks `);
+        location.reload();  
+    }
+})
+
